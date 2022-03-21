e72e3fc0132c86f432199085cf521e41003d3c84
elmm-programing <86208465+elmm-programing@users.noreply.github.com>
2022-03-21 13:22:18 -0400

    1.2.2

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import {LoginService} from 'src/app/services/login.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/compat/database';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import {Observable} from 'rxjs';
 @Component({
@@ -11,10 +12,29 @@ import {Observable} from 'rxjs';
 export class MainComponent implements OnInit {
   UserLogged = this.loginService.getUserLogged(); 
   UserId:string|null|undefined;
-  profileUrl: Observable<string | null>;
-  ref:any;
-  constructor(private loginService: LoginService,private route: ActivatedRoute,private _router: Router,private storage: AngularFireStorage) {
-    this.UserId = this.route.snapshot.paramMap.get('uid');
+  profileUrl: Observable<string | null> 
+  nameItemRef: AngularFireObject<any>;
+  name: Observable<any>;;
+  comentarioItemRef: AngularFireObject<any>;
+  comentario: Observable<any>;;
+
+  constructor(private loginService: LoginService,private route: ActivatedRoute,private _router: Router,private storage: AngularFireStorage,db: AngularFireDatabase) {
+
+         this.UserId = this.route.snapshot.paramMap.get('uid');
+
+	 this.nameItemRef = db.object(`usuarios/${this.UserId}/name`);
+    this.name = this.nameItemRef.valueChanges();
+    
+	 this.comentarioItemRef = db.object(`usuarios/${this.UserId}/comentario`);
+    this.comentario = this.comentarioItemRef.valueChanges();
+
+
+
+/*
+    const itemPath =  `usuarios/${this.UserId}`;
+      this.item = db.object(itemPath).valueChanges();
+      console.log(this.item);
+*/
     console.log(this.UserId);
 const ref = this.storage.ref(`/users/${this.UserId}`);
      this.profileUrl = ref.getDownloadURL();
